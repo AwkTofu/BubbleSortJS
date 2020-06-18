68347d337257d184544f5d9d4e761a29d23bc0dc
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-06-18 14:05:26 -0400

    bubble sort finished

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,1 +1,26 @@
 console.log("Starting Bubble Sort");
+
+var arr = [4,2,3,5,1]
+
+function bubbleSort(unsortedArray) {
+	let length = unsortedArray.length;
+
+	for (let i = 0; i < length; i++)
+	{
+		for (let j = 0; j < length; j++)
+		{
+			if (unsortedArray[j] > unsortedArray[j + 1])
+			{
+				let temp = unsortedArray[j];
+				unsortedArray[j] = unsortedArray[j + 1];
+				unsortedArray[j + 1] = temp;
+			}
+		}
+	}
+
+	return unsortedArray;
+}
+
+console.log("Before: " + arr);
+console.log("Result: " + bubbleSort(arr));
+console.log("After: " + arr);
